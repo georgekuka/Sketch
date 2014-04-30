b04947361a627b5e8bd4680d2b95769e6bdca35e
George Kuka <gk3biz@gmail.com>
2014-04-30 13:18:07 -0700

    add title and styling

---
+++ sketch.js
@@ -0,0 +1,8 @@
+$container = $('contain_divs');
+$solid = $('#tool_1');
+$rainbow = $('#tool_2');
+$special = $('#tool_3');
+
+$(document).ready(function() {
+	confirm("Please select a tool and then enter a grid size.");
+});
