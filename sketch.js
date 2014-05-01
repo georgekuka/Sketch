d1776a8c0ec882e9a327879b4f73711ab1669f50
George Kuka <gk3biz@gmail.com>
2014-05-01 08:54:43 -0700

    fixed tools and clear grid

--- sketch.js
+++ sketch.js
@@ -2,23 +2,20 @@ $container = $('.contain_divs');
 $solid = $('#tool_1');
 $rainbow = $('#tool_2');
 $special = $('#tool_3');
-$activeTool = 0;
+var activeTool = 0;
 var size = 0;
-
-function clearBoard(size) {
-	createBoard(size);
-};
 
 function createBoard(clearNum) {
 	// When we create a board, we don't want to have multiples. So the first thing we do is empty the container.
 	$('.contain_divs').empty();
-	if (typeof clearNum === "undefined") {
-	// When we create our grid, we save it's size to a variable. So if we want to just clear the board of any coloring,
-	// we don't have to reprompt the user for a size. The function createBoard takes an argument "clearNum" as an optional
-	// argument that only the function clearBoard(); will pass in. Select any of the tools will not pass in an argument,
-	// triggering a prompt. 
+	// Next we check if an argument has been passed. The only function we want to pass an argument to createBoard
+	// is clearBoard, since we're not creating a new board and loading a new tool, we're simply clearing the board
+	// of any color. So if the argument is empty, we prompt for a new grid size.
+		if (typeof clearNum === "undefined") { 
 	temp = prompt("Please enter a grid size, 1 - 128.");
-	size = parseInt(temp); } else {
+	size = parseInt(temp); } 
+		else {
+		// This else is trigger is an argument was present and will set the gridsize to the argument.
 		size = clearNum;
 	}
 	// Check if grid is between 1 and 128, if not prompt for another number.
@@ -34,16 +31,44 @@ function createBoard(clearNum) {
 	for (i = (size * size); i > 0; i--) {
 		$("<div class='block' style='width: " + width + "px; height: " + height + "px;'>").appendTo('.contain_divs');
 	};
+	switch (activeTool) {
+		case 0:
+			break;
+		case 1:
+			$('.block').mouseenter(function() {
+				$(this).css('background-color', 'red');
+			}); break;
+		case 2:
+			$('.block').mouseenter(function() {
+				$(this).css('background-color', 'rgb('
+												+ (Math.floor(Math.random() * 256)) + ','
+												+ (Math.floor(Math.random() * 256)) + ','
+												+ (Math.floor(Math.random() * 256)) + ')');
+			}); break;
+		case 3:
+			$('.block').mouseenter(function() {
+				$(this).css('background-color', 'red');
+			}); break;
+		default:
+			break;	 	
+	}
 };
 
 function solidDraw() {
+	activeTool = 1;
 	createBoard();
 };
 
 function rainbowDraw() {
+	activeTool = 2;
 	createBoard();
 };
 
 function specialDraw() {
+	activeTool = 3;
 	createBoard();
+};
+
+function clearBoard(size) {
+	createBoard(size);
 };
